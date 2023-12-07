e95165e388f3ff9b8bcfbf9ee53a6fab2e17f880
muhammedbayhan <76851132+muhammedbayhan@users.noreply.github.com>
2023-12-07 20:50:15 +0300

    service

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,10 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_weather_app/models/weather_model.dart';
+
 class WeatherService {
+  Future<WeatherModel?> fetchWeatherData(String? city) async {
+    String endpoint =
+        "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=2e37bceb4ed6a24ee3ce101dd2447d8d&units=metric";
+    final response = await Dio().get(endpoint);
 
-  //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    if (response.statusCode == 200) {
+      final _datas = response.data;
+      print(WeatherModel.fromJson(_datas).name);
+      return WeatherModel.fromJson(_datas);
+    }
 
-void getData (){
-
-  
-}
-
+    return null;
+  }
 }
